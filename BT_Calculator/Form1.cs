469be23618d32e4854b5111c9504e9dd45a6deb7
tien <1551010131tien@gmail.com>
2017-10-03 14:15:01 +0700

    BTsudungGithub

---
+++ BT_Calculator/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BT_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToInt16(textBox1.Text);
+            double b = Convert.ToInt16(textBox1.Text);
+            double c = a + b;
+            textBox3.Text = c.ToString();
+        }
+    }
+}
